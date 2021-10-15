fc82d78d1c20b39efe1a2ab0dfbb9debb86527d9
mas <m@stdwtr.io>
2021-10-15 15:07:02 -0400

    More vs code configurations

--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,47 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(void) {
-  std::cout << "Starting..." << std::endl;
+int main(void)
+{
+    std::cout << "Starting..." << std::endl;
 
-  GLFWwindow *window;
+    GLFWwindow* window;
 
-  /* Initialize the library */
-  if (!glfwInit())
-    return -1;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-  /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "OGL", NULL, NULL);
-  if (!window) {
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "OGL", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    /* THIS IS A VALID GL CONTEXT STARTING NOW */
+    glfwMakeContextCurrent(window);
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
     glfwTerminate();
-    return -1;
-  }
-
-  /* Make the window's context current */
-  /* THIS IS A VALID GL CONTEXT STARTING NOW */
-  glfwMakeContextCurrent(window);
-
-  std::cout << glGetString(GL_VERSION) << std::endl;
-
-  /* Loop until the user closes the window */
-  while (!glfwWindowShouldClose(window)) {
-    /* Render here */
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(0, 0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glEnd();
-
-    /* Swap front and back buffers */
-    glfwSwapBuffers(window);
-
-    /* Poll for and process events */
-    glfwPollEvents();
-  }
-
-  glfwTerminate();
-  return 0;
+    return 0;
 }
