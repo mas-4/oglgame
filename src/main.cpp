45a9e5b774f82ab9f169c4068367d4fb0d53821c
mas <m@stdwtr.io>
2021-10-15 17:51:34 -0400

    Some shader functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,47 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+/* Compiles a shader, `source`,  of type `type` and returns the id */
+static unsigned int compile_shader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile shader!" << (type == GL_VERTEX_SHADER ? "vertex": "fragment") << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return -1;
+    }
+
+    return id;
+}
+
+/* creates a shader with `vertex_shader` and `fragment_shader` and ties it to the program. Returns program id */
+static unsigned int create_shader(const std::string& vertex_shader, const std::string& fragment_shader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compile_shader(GL_VERTEX_SHADER, vertex_shader);
+    unsigned int fs = compile_shader(GL_FRAGMENT_SHADER, fragment_shader);
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     std::cout << "Starting..." << std::endl;
@@ -11,7 +52,6 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "OGL", NULL, NULL);
@@ -31,23 +71,21 @@ int main(void)
 
     float positions[6] = {
         -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+        0.0f, 0.5f,
+        0.5f, -0.5f
     };
 
     unsigned int buffer_id;
     glGenBuffers(1, &buffer_id);
     glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*6, positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, positions, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
