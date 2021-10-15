e7b1270f40bc7e3c6a8b1398e2790ce2faa1ab7d
mas <m@stdwtr.io>
2021-10-15 15:59:32 -0400

    Modern opengl triangle!

--- src/main.cpp
+++ src/main.cpp
@@ -12,33 +12,47 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "OGL", NULL, NULL);
+
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
-    /* THIS IS A VALID GL CONTEXT STARTING NOW */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK)
+        std::cout << "glewInit failed" << std::endl;
+
     std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer_id;
+    glGenBuffers(1, &buffer_id);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*6, positions, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
         /* Poll for and process events */
         glfwPollEvents();
     }
