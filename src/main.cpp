8af626c8e1b72b26eef7aadfadbe5dc7315e8b16
mas <m@stdwtr.io>
2021-10-16 13:52:43 -0400

    Indices buffer

--- src/main.cpp
+++ src/main.cpp
@@ -123,26 +123,33 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    // vertices of the triangle
+    /* vertices of the triangle */
     // clang-format off
-    float positions[6*2] = {
+    float positions[] = {
         -0.5f, -0.5f,
          0.5f, -0.5f,
          0.5f,  0.5f,
-
-         0.5f,  0.5f,
         -0.5f,  0.5f,
-        -0.5f, -0.5f
+    };
+    unsigned int indices[] {
+        0, 1, 2,
+        2, 3, 0
     };
     // clang-format on
 
-    // initialize my vertex buffer
+    /* initialize the vertex buffer */
     unsigned int buffer_id;
     glGenBuffers(1, &buffer_id);
     glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
     glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     // set up the shader
     ShaderProgramSource source = parse_shader(BASIC_SHADER);
@@ -154,7 +161,7 @@ int main(void)
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
