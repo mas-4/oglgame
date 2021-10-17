7060f4577323696bc236b48f64e121d189361d5c
mas <m@stdwtr.io>
2021-10-17 16:14:38 -0400

    Another uniform method

--- src/Shader.h
+++ src/Shader.h
@@ -25,6 +25,8 @@ public:
   void Unbind() const;
 
   // Set uniforms
+  void SetUniform1f(
+      const std::string& name, float v0);
   void SetUniform4f(
       const std::string& name, float v0, float v1, float v2, float v3);
 
