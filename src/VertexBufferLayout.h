12623c310df560661855398dbd0b56a456f04320
mas <m@stdwtr.io>
2021-10-17 13:24:43 -0400

    Template specialization works in c++17 with clang

--- src/VertexBufferLayout.h
+++ src/VertexBufferLayout.h
@@ -32,24 +32,24 @@ public:
   VertexBufferLayout()
       : m_Stride(0) {};
 
-  template <typename T> void Push(int count) { static_assert(false); }
+  template <typename T> void Push(unsigned int count) { static_assert(false); }
 
   template <> void Push<float>(unsigned int count)
   {
-    m_Elements.PushBack({ GL_FLOAT, count, GL_FALSE });
+    m_Elements.push_back({ GL_FLOAT, count, GL_FALSE });
     m_Stride += VertexBufferElement::GetSizeOfType(GL_FLOAT);
   }
 
   template <> void Push<unsigned int>(unsigned int count)
   {
-    m_Elements.PushBack({ GL_UNSIGNED_INT, count, GL_FALSE });
+    m_Elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
     m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
   }
 
   template <> void Push<unsigned char>(unsigned int count)
   {
-    m_Elements.PushBack({ GL_UNSIGNED_BYTE, count, GL_TRUE });
-    m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYLTE);
+    m_Elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
+    m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
   }
 
   inline const std::vector<VertexBufferElement> GetElements() const
